88dcdd0bb17099f868221cdf94594947255eabb7
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-09-08 18:13:53 +0200

    Luarocks disabled

--- .config/nvim/lua/lazy-init.lua
+++ .config/nvim/lua/lazy-init.lua
@@ -49,6 +49,9 @@ require('lazy').setup({
     },
   },
 }, {
+  rocks = {
+    enabled = false,
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
