0632e4a88440582db9b9a3c990c3390bc3bcfdd9
jknezevic-cirtuo <jakov.knezevic@cirtuo.com>
2026-09-08 17:04:11 +0200

    Work layout update

--- .config/wezterm/commands/work-layout.lua
+++ .config/wezterm/commands/work-layout.lua
@@ -1,16 +1,16 @@
 local wezterm = require 'wezterm'
 
 local command = {
-  brief = 'Open GSC work environment tabs',
+  brief = 'Open work environment',
   icon = 'md_window_maximize',
   action = wezterm.action_callback(function(window, pane)
     local home = os.getenv 'HOME'
     local wezterm_bin = '/Applications/WezTerm.app/Contents/MacOS/wezterm'
-    
+
     -- Get the unique ID of the window you are currently looking at
     local current_window_id = tostring(window:window_id())
 
-    -- 1. Spawn the GSC tab into the current window
+    -- 1. Spawn the first tab into the current window
     wezterm.run_child_process {
       wezterm_bin,
       'cli',
@@ -21,7 +21,7 @@ local command = {
       home .. '/Work/GSC',
     }
 
-    -- 2. Spawn the GSCLite tab into the current window
+    -- 2. Spawn the second tab into the current window
     wezterm.run_child_process {
       wezterm_bin,
       'cli',
@@ -32,7 +32,7 @@ local command = {
       home .. '/Work/GSC/server/GSCLite',
     }
 
-    -- 3. Spawn the web-workspace tab into the current window
+    -- 3. Spawn the third tab into the current window
     wezterm.run_child_process {
       wezterm_bin,
       'cli',
@@ -41,6 +41,17 @@ local command = {
       current_window_id,
       '--cwd',
       home .. '/Work/GSC/web-workspace',
+    }
+
+    -- 4. Spawn the fourth tab into the current window
+    wezterm.run_child_process {
+      wezterm_bin,
+      'cli',
+      'spawn',
+      '--window-id',
+      current_window_id,
+      '--cwd',
+      home .. '/Work/GSC',
     }
   end),
 }
