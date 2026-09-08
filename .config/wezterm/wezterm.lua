3e5d126e244f51ee5330bed71ec9a7a77e28d393
Jakov Knezevic <knezevic.jakov@gmail.com>
2026-09-08 17:44:51 +0200

    Font size update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 config.default_prog = { 'powershell.exe', '-NoLogo' }
 
 -- Font settings
-config.font_size = 11.5
+config.font_size = 10
 config.line_height = 1.5
 config.font = wezterm.font('Fira Code', { italic = true, weight = 700 })
 
