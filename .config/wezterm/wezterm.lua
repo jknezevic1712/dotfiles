88517b87f30ab8e9460447bb975af5d0a886a587
jknezevic-cirtuo <jakov.knezevic@cirtuo.com>
2026-09-09 10:08:45 +0200

    Blur and opacity update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,8 +22,8 @@ config.window_padding = {
   top = 10,
   bottom = 10,
 }
-config.macos_window_background_blur = 40
-config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+config.window_background_opacity = 0.85
 config.window_background_image = constants.bg_image
 config.tab_bar_at_bottom = true
 
