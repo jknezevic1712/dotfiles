d7069d0c8c77728ff09ada48a2aaedfe2d274e44
jknezevic-cirtuo <jakov.knezevic@cirtuo.com>
2026-09-25 13:36:56 +0200

    Misc

--- .config/nvim/lua/plugins/editor/ufo.lua
+++ .config/nvim/lua/plugins/editor/ufo.lua
@@ -16,7 +16,7 @@ return {
         end,
       },
       {
-        'K',
+        '<leader>k',
         function()
           local winid = require('ufo').peekFoldedLinesUnderCursor()
           if not winid then
